d597b6aca4086625245f39876c3fa1e0f163e87b
code-turpal <gireevanzor@gmail.com>
2023-09-14 19:49:34 +0300

    commit bhj-diploma

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -4,10 +4,16 @@
  * Имеет свойство URL со значением '/account'
  * */
 class Account extends Entity {
-  /**
-   * Получает информацию о счёте
-   * */
-  static get(id = '', callback){
-
+    /**
+     * Получает информацию о счёте
+     * */
+    static URL = '/account';
+  
+    static get(id = '', callback){
+      createRequest({
+        url: `${this.URL}/${id}`,
+        method: 'GET',
+        callback: callback
+      })
+    }
   }
-}
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,46 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-
-};
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+  
+    let url = options.url;
+    let formData = new FormData();
+  
+    if (options.data) {
+      if (options.method === 'GET') {
+        url += `?${Object.entries(options.data)
+          .map(
+            ([key, value]) =>
+              `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+          )
+          .join('&')}`;
+      } else {
+        Object.entries(options.data).forEach((keyValue) =>
+          formData.append(...keyValue)
+        );
+      }
+    }
+  
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        let err = null;
+        let response = null;
+  
+        if (xhr.status === 200) {
+          if (xhr.response && xhr.response.success) {
+            response = xhr.response;
+          } else {
+            err = xhr.response;
+          }
+        } else {
+          err = new Error('Что-то пошло не так');
+        }
+  
+        options.callback(err, response);
+      }
+    };
+  
+    xhr.open(options.method, url);
+    xhr.send(formData);
+  };
